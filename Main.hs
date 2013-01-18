d37f6d7a1f74c78a70946275f511dc157ff2833e
Gaël Deest <gael.deest@gmail.com>
2013-01-18 20:08:14 +0100

    Full Conduit

--- Main.hs
+++ Main.hs
@@ -26,48 +26,42 @@ import Database.HDBC
 import Database.HDBC.PostgreSQL
 
 
-dbBackend :: Chan WMRMessage -> IO ()
-dbBackend msgChan = do conn <- connectPostgreSQL "user=gael dbname=wmr100"
-                       feedDB conn msgChan
-
-feedDB :: Connection -> Chan WMRMessage -> IO ()
-feedDB c chan = do
+mkDbBackend :: Connection -> IO (WMRMessage -> IO ())
+mkDbBackend c = do
   insertWindSample <- prepare c "INSERT INTO wind_samples VALUES (?,?,?,?)"
   insertTempSample <- prepare c "INSERT INTO temp_samples VALUES (?,?,?)"
   insertRHSample <- prepare c "INSERT INTO relative_humidity_samples VALUES (?,?,?)"
   insertPressureSample <- prepare c "INSERT INTO pressure_samples VALUES (?,?)"
   insertForecastSample <- prepare c "INSERT INTO forecast_samples VALUES (?,?)"
-  forever $ do
-    msg <- readChan chan
-    at <- fmap toSql getCurrentTime
-    print msg
-    case msg of
-      BarometerMsg fcast pressure -> do
-                  execute insertPressureSample [ at, toSql pressure ]
-                  execute insertForecastSample [ at, toSql $ fromEnum fcast ]
-                  return ()
-      TempHumidityMsg sensor temp humidity -> do
-                  execute insertTempSample [ at, toSql sensor, toSql temp ]
-                  execute insertRHSample [ at, toSql sensor, toSql humidity ]
-                  return ()
-      AnenometerMsg dir gust avg -> do
-                  execute insertWindSample [ at
-                                           , toSql $ fromEnum dir
-                                           , toSql gust
-                                           , toSql avg
-                                           ]
-                  return ()
-      _ -> return ()
-    commit c
+  return $ (\msg ->  do
+              at <- fmap toSql getCurrentTime
+              print msg
+              case msg of
+                BarometerMsg fcast pressure -> do
+                         execute insertPressureSample [ at, toSql pressure ]
+                         execute insertForecastSample [ at, toSql $ fromEnum fcast ]
+                         return ()
+                TempHumidityMsg sensor temp humidity -> do
+                         execute insertTempSample [ at, toSql sensor, toSql temp ]
+                         execute insertRHSample [ at, toSql sensor, toSql humidity ]
+                         return ()
+                AnenometerMsg dir gust avg -> do
+                         execute insertWindSample [ at
+                                                  , toSql $ fromEnum dir
+                                                  , toSql gust
+                                                  , toSql avg
+                                                  ]
+                         return ()
+                _ -> return ()
+              commit c
+           )
 
-
-threadWrapper :: MVar SomeException -> IO () -> IO ()
-threadWrapper mvar action = do forkIO $ catch action (\(e::SomeException) -> putMVar mvar e)
-                               return ()
-
-justReadMessages :: MonadIO m => (WMRMessage -> IO ()) -> Sink WMRMessage m ()
-justReadMessages action = awaitForever onMessage
+foreachMessage :: MonadIO m => (WMRMessage -> IO ()) -> Sink WMRMessage m ()
+foreachMessage action = awaitForever onMessage
   where onMessage m = liftIO $ action m
 
 main :: IO ()
-main = runResourceT $ wmr100source $$ justReadMessages print
+main = withPostgreSQL "user=gael dbname=wmr100" go
+    where go conn = do 
+            backend <- mkDbBackend conn
+            runResourceT $ wmr100source $$ foreachMessage backend
--- System/USB/WMRX00.hs
+++ System/USB/WMRX00.hs
@@ -1,20 +1,20 @@
 module System.USB.WMRX00 ( WMRMessage(..)
                          , WeatherForecast(..)
                          , WindDirection(..)
-                         , feedWMRMessageChan
                          , wmr100source
                          ) where
 
+import Prelude hiding (take, init, last, length, filter, foldl)
 import Control.Monad
 import Control.Monad.IO.Class
 import System.USB.HID
-import qualified Data.ByteString as B
-import Data.Conduit (await, yield, ($=), (=$=))
+import Data.ByteString (ByteString, take, init, last, length, uncons, pack, foldl)
+import Data.Conduit (await, yield, bracketP, ($=), (=$=), GSource, Source, GInfConduit, MonadResource)
+import Data.Conduit.List (mapMaybe, concatMapAccum, filter)
 import qualified Data.Conduit as C
 import Data.Conduit.Binary hiding (take)
-import qualified Data.Conduit.List as L (foldM)       
 import Control.Monad.IO.Class (liftIO)
-import Text.Parsec.Prim
+import Text.Parsec.Prim hiding (uncons)
 import Text.Parsec.Char
 import Text.Parsec.ByteString
 import Text.Parsec.Combinator
@@ -95,87 +95,63 @@ msgParser :: Parser WMRMessage
 msgParser = do anyChar
                dateTimeP <|> barometerP <|> tempHumidityP <|> anenometerP
 
-computeCheckSum :: B.ByteString -> Word8
-computeCheckSum msg = fromInteger $ B.foldl (\a c -> a + fromIntegral (fromEnum c)) 0 msg .&. 255
-
-usbUnwrap :: Monad m => C.GConduit B.ByteString m B.ByteString
-usbUnwrap = loop
-    where loop = do await >>= (maybe loop $ \packet -> case B.uncons packet of
-                      Nothing       -> loop
-                      Just (hd, tl) -> let len = fromIntegral hd
-                                           sub = B.take len tl in
-                                       if len > B.length tl then loop
-                                       else C.yield sub >> loop)
-
-usbPackets :: MonadIO m => HidDevice -> C.GSource m B.ByteString
+-- | Produces a stream of (normally) eight bytes 'ByteString' packets.
+usbPackets :: MonadIO m => HidDevice -> GSource m ByteString
 usbPackets dev = loop
   where loop = do packet <- liftIO (hidRead dev 8)
-                  C.yield packet >> loop
+                  yield packet >> loop
 
-processMessage :: MonadIO m => Chan WMRMessage -> B.ByteString -> m ()
-processMessage msgChan str = let (msg', msg, cs) = (B.init str, B.init msg', B.last msg') in
-              if computeCheckSum msg /= cs then liftIO $ putStrLn "Error: Invalid CheckSum."
-              else case parse msgParser "" msg of
-                     Left err -> liftIO $ print err
-                     Right m  -> do liftIO $ print m
-                                    liftIO $ writeChan msgChan m
+-- | Unwraps packets, stripping non-data bytes.
+-- The first byte of a USB packet indicates the number of data bytes it contains.
+-- This conduit reads this value and extracts the right number of bytes from the
+-- remaining string.
+unwrap :: Monad m => GInfConduit ByteString m ByteString
+unwrap = mapMaybe unwrapSingle
+    where unwrapSingle msg =
+              case uncons msg of
+                Nothing       -> Nothing
+                Just (hd, tl) -> let len = fromIntegral hd in
+                                 if len > length tl then Nothing
+                                 else Just $ take len tl
 
+-- | Used internally to keep track of sync state.
 data ParseState = SYNC | FIRST | READY [Word8]
-splitStream :: Monad m => C.GConduit B.ByteString m B.ByteString
-splitStream = awaitWithState SYNC
-  where awaitWithState s = C.await >>= maybe (return ()) (wmrDecoder' s)
-        wmrDecoder' s i =
-          case B.uncons i of
-            Nothing       -> awaitWithState s
-            Just (hd, tl) -> case s of
-              SYNC      -> wmrDecoder' (if hd == 0xff then FIRST    else SYNC) tl
-              FIRST     -> wmrDecoder' (if hd == 0xff then READY [] else SYNC) tl
-              READY acc -> if hd == 0xff then do C.yield $ B.pack $ reverse acc
-                                                 wmrDecoder' SYNC tl
-                           else wmrDecoder' (READY $ hd:acc) tl
 
--- processMessage :: MonadIO m => Chan WMRMessage -> B.ByteString -> m ()
--- processMessage msgChan str = let (msg', msg, cs) = (B.init str, B.init msg', B.last msg') in
---               if computeCheckSum msg /= cs then liftIO $ putStrLn "Error: Invalid CheckSum."
---               else case parse msgParser "" msg of
---                      Left err -> liftIO $ print err
---                      Right m  -> do liftIO $ print m
---                                     liftIO $ writeChan msgChan m
+-- | Extracts messages from a 'ByteString' stream.
+-- In (unwrapped) USB data, messages are separated by two 0xff bytes.
+-- This conduit splits the incoming stream in messages.
+splitStream :: Monad m => GInfConduit ByteString m ByteString
+splitStream = concatMapAccum splitter SYNC
+  where splitter packet state =
+          case uncons packet of
+            Nothing       -> (SYNC, [])
+            Just (hd, tl) -> foldl iter (state, []) packet
+        iter (parseState, acc) c =
+          case parseState of
+            SYNC     -> if c == 0xff then (FIRST,    acc) else (SYNC, acc)
+            FIRST    -> if c == 0xff then (READY [], acc) else (SYNC, acc)
+            READY ws -> if c == 0xff then (FIRST, acc ++ [pack ws])
+                        else (READY $ ws++[c], acc) 
 
-parseMessages :: Monad m => C.GConduit B.ByteString m WMRMessage
-parseMessages = loop
-  where loop = do res <- await
-                  case res of
-                    Nothing -> loop
-                    Just msg -> case parse msgParser "" msg of
-                      Left err -> loop
-                      Right m  -> yield m >> loop
+-- | Simple checksum computation for WMR100.
+-- Checksum computation is simply an addition modulo 256.
+computeCheckSum :: ByteString -> Word8
+computeCheckSum msg = fromInteger $ foldl (\a c -> a + fromIntegral (fromEnum c)) 0 msg .&. 255
 
-filterBadMessages :: Monad m => C.GConduit B.ByteString m B.ByteString
-filterBadMessages = loop
-  where loop = do s <- await
-                  case s of Nothing  -> return ()
-                            Just str -> let (msg', msg, cs) = (B.init str, B.init msg', B.last msg') in
-                              if computeCheckSum msg /= cs then loop
-                              else yield msg >> loop
+-- | Performs a checksum test on each message, dropping corrupt ones.
+filterBadMessages :: Monad m => GInfConduit ByteString m ByteString
+filterBadMessages = filter validChecksum
+    where validChecksum m = let (msg', msg, cs) = (init m, init msg', last msg') in
+                            computeCheckSum msg == cs
 
-wmr100source :: C.MonadResource m => C.Source m WMRMessage
-wmr100source = C.bracketP (hidOpen 0x0fde 0xca01) hidClose usbPackets $= process
-  where process = usbUnwrap =$= splitStream =$= filterBadMessages =$= parseMessages
+-- | Parses a stream of 'ByteString's into a stream of 'WMRMessage's.
+parseMessages :: Monad m => GInfConduit ByteString m WMRMessage
+parseMessages = mapMaybe parseSingle
+    where parseSingle str = case parse msgParser "" str of
+                              Left err  -> Nothing
+                              Right msg -> Just msg
 
-
-feedWMRMessageChan :: MonadIO m => Chan WMRMessage -> m ()
-feedWMRMessageChan chan = do dev <- liftIO $ hidOpen 0x0fde 0xca01
-                             usbPackets dev C.$$ sinkUsb
-                             return ()
-    where sinkUsb = L.foldM iter SYNC
-          iter s p = if p == B.empty then return s
-                     else let (hd:tl) = B.unpack p in
-                          foldM step s $ take (fromIntegral hd) tl
-          step state c = 
-              case state of
-                SYNC  -> return $ if c == 0xff then FIRST else state
-                FIRST -> return $ if c == 0xff then READY [] else SYNC
-                READY acc -> if c == 0xff then do processMessage chan $ B.pack $ reverse acc
-                                                  return SYNC
-                             else return $ READY $ c:acc
+-- | Connects to WMR100 weather station on USB and produces a stream of messages.
+wmr100source :: MonadResource m => Source m WMRMessage
+wmr100source = bracketP (hidOpen 0x0fde 0xca01) hidClose usbPackets $= process
+  where process = unwrap =$= splitStream =$= filterBadMessages =$= parseMessages
